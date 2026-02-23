e1ebb2a6cddf947b6075c25d11909e08ab3120ae
Hasan Sabri <hsabri2007@gmail.com>
2026-02-23 21:56:09 +0000

    view posts

--- profile.php
+++ profile.php
@@ -5,6 +5,8 @@ session_start();
 if (!isset($_COOKIE["USER_NAME"])) {
     header("Location: login.php");
 }
+
+$db = new SQLite3("MessageBoard.db");
 ?>
 
 <html>
@@ -16,5 +18,19 @@ if (!isset($_COOKIE["USER_NAME"])) {
 	    <h2><?php echo $_COOKIE["USER_NAME"]; ?>'s profile</h2>
 					<a href="post.php">Make a post</a>
 		<a href="logout.php">Log out</a>
+<?php
+$fetch_posts_query = $db->prepare(
+    "SELECT * FROM posts WHERE user_id = ? limit 10",
+);
+$fetch_posts_query->bindValue(1, $_COOKIE["USER_ID"]);
+$posts = $fetch_posts_query->execute();
+while ($post = $posts->fetchArray()) {
+    echo "<div class='post'>";
+    echo "<p>" . $post["content"] . "</p>";
+    echo "<p>" . $post["created_at"] . "</p>";
+    echo "</div>";
+}
+?>
+
 	</body>
 </html>
