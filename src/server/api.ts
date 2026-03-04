265932d0488108b80c5acc0d3b7e20237cb40e2d
Hasan Sabri <hsabri2007@gmail.com>
2026-03-04 00:07:01 +0000

    work on api

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,4 +1,4 @@
-import { serve } from "bun";
+import { createUser, getPosts, getUserByUsername } from "./db";
 
 export default {
 	"/api/greet": {
@@ -6,14 +6,55 @@ export default {
 			return Response.json({ hello: "world" });
 		},
 	},
-	"/api/login": {
+	"/api/user/login": {
 		async POST(req: Request) {
 			const { username, password } = await req.json();
+			if (!username || !password) {
+				return Response.json(
+					{ error: "username and password are required" },
+					{ status: 400 },
+				);
+			}
+
+			const user = getUserByUsername(username);
+			if (!user || user.password !== password) {
+				return Response.json(
+					{ error: "invalid username or password" },
+					{ status: 400 },
+				);
+			}
+
+			return Response.json({ success: true, user });
 		},
 	},
-	"/api/register": {
+	"/api/user/register": {
 		async POST(req: Request) {
 			const { username, password } = await req.json();
+			if (!username || !password) {
+				return Response.json(
+					{ error: "username and password are required" },
+					{ status: 400 },
+				);
+			}
+
+			const checkExists = getUserByUsername(username);
+			if (checkExists) {
+				return Response.json(
+					{ error: "username already exists" },
+					{ status: 400 },
+				);
+			}
+
+			createUser(username, password);
+			const user = getUserByUsername(username);
+			return Response.json({ success: true, user });
+		},
+	},
+	"/api/posts/fetch": {
+		async GET(req: Request) {
+			const { begin, count } = await req.json();
+			const posts = getPosts(begin, count);
+			return Response.json({ posts });
 		},
 	},
 };
--- src/server/db.ts
+++ src/server/db.ts
@@ -1,6 +1,6 @@
 import sqlite from "node:sqlite";
 
-const db = new sqlite.DatabaseSync("db.sqlite");
+export const db = new sqlite.DatabaseSync("db.sqlite");
 
 db.exec(`CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -14,5 +14,45 @@ db.exec(`CREATE TABLE IF NOT EXISTS posts (
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id)
 )`);
+
+export function getUserById(id: number) {
+	const user = db.prepare(`SELECT * FROM users WHERE id = ?`).get(id);
+	return user;
+}
+
+export function getUserByUsername(username: string) {
+	const user = db
+		.prepare(`SELECT * FROM users WHERE username = ?`)
+		.get(username);
+	return user;
+}
+
+export function createUser(username: string, password: string) {
+	const user = db
+		.prepare(`INSERT INTO users (username, password) VALUES (?, ?)`)
+		.run(username, password);
+	return user;
+}
+
+export function login(username: string, password: string) {
+	const user = db
+		.prepare(`SELECT * FROM users WHERE username = ? AND password = ?`)
+		.get(username, password);
+	return user;
+}
+
+export function getPosts(begin: number, count: number) {
+	const posts = db
+		.prepare(`SELECT * FROM posts WHERE id > ? ORDER BY id ASC LIMIT ?`)
+		.all(begin, count);
+	return posts;
+}
+
+export function createPost(userId: number, content: string) {
+	const post = db
+		.prepare(`INSERT INTO posts (user_id, content) VALUES (?, ?)`)
+		.run(userId, content);
+	return post;
+}
 
 export default db;
