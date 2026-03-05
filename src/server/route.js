42cc1e00abd4a59e3b3df4a879b0e3f5391ccb3c
Hasan Sabri <hsabri2007@gmail.com>
2026-03-05 18:01:30 +0000

    again

--- src/server/route.js
+++
@@ -1,9 +0,0 @@
-export default {
-	"/api": {
-		GET: () => {
-			return new Response(
-				JSON.stringify({ success: true, message: "Hello, API!" }),
-			);
-		},
-	},
-};
--- src/server/utils.js
+++
@@ -1,6 +0,0 @@
-import path from "path";
-
-const clientPath = path.join(import.meta.dirname, "../client");
-
-export { clientPath };
-export default { clientPath };
