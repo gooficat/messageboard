9c14c28e37726eb1d1bb07927bc1a7e08b959a7e
Hasan Sabri <hsabri2007@gmail.com>
2026-03-05 00:15:53 +0000

    sane

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,16 @@
-import http from "http";
+import bun from "bun";
 import route from "./route.js";
-import db from "./db.js";
+import utils from "./utils.js";
 
-const server = http.createServer((req, res) => route.route(req, res));
-
-server.listen(process.env.PORT, () => {
-	console.log(`Server listening on port ${process.env.PORT}`);
+const server = bun.serve({
+	port: process.env.PORT,
+	routes: route,
+	async fetch(req) {
+		const url = new URL(req.url);
+		const file = bun.file(`${utils.clientPath}/${url.pathname}`);
+		if (await file.exists()) {
+			return new Response(file);
+		}
+		return new Response(bun.file(`${utils.clientPath}/index.html`));
+	},
 });
--- src/server/utils.js
+++ src/server/utils.js
@@ -1,23 +1,6 @@
-import http from "http";
-import fs from "fs";
 import path from "path";
-import url from "url";
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const clientPath = path.join(__dirname, "../client");
+const clientPath = path.join(import.meta.dirname, "../client");
 
-function sendFile(filePath, res) {
-	fs.readFile(filePath, (err, data) => {
-		if (err) {
-			res.writeHead(404);
-			res.end("File not found");
-		} else {
-			res.writeHead(200);
-			res.end(data);
-		}
-	});
-}
-
-export default { __filename, __dirname, clientPath, sendFile };
-export { __filename, __dirname, clientPath, sendFile };
+export { clientPath };
+export default { clientPath };
